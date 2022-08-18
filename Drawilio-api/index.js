cfd2b679ab29f2299c6c9982aa6d86870cb15bc3
Hamza <96361868+Hamziss@users.noreply.github.com>
2022-08-18 18:09:43 +0100

    Update index.js

--- Drawilio-api/index.js
+++ Drawilio-api/index.js
@@ -10,22 +10,20 @@ socket.init(ioServer);
 
 app.use((req, res, next) => {
 
-    // Website you wish to allow to connect
+   
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
+    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
+    
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Pass to next layer of middleware
+    
     next();
 });
 
-/**
- * To check if service is alive or not
- */
+
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
