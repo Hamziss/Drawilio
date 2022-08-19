3539cc1512230e619d7b01ec13e5d42e804b899c
Hamza <96361868+Hamziss@users.noreply.github.com>
2022-08-19 03:27:10 +0100

    Update index.js

--- Drawilio-api/index.js
+++ Drawilio-api/index.js
@@ -8,18 +8,12 @@ const app = express();
 const ioServer = new http.createServer(app);
 socket.init(ioServer);
 
-app.use((req, res, next) => {
-
-   
+app.use((req, res, next) => {   
     res.setHeader('Access-Control-Allow-Origin', '*');
-
     
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');    
     next();
 });
 
